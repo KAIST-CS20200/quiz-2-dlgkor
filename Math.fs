88270fcac5abe5a6d9a3f9d197ab7684c41c3f13
ydhan-mac-pro <ydhan1024@gmail.com>
2025-03-11 10:07:49 +0900

    commit

--- Math.fs
+++ Math.fs
@@ -6,4 +6,9 @@ open System
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
 let collatz n =
-  0
+  
+  let isEven n = (n%2UL=0UL)
+  let rec collatz_rec n ct = if n=1UL then ct 
+                             elif isEven n then (collatz_rec (n/2UL) (ct+1))
+                             else (collatz_rec (3UL*n+1UL) (ct+1))
+  collatz_rec (uint64 n) 0   
